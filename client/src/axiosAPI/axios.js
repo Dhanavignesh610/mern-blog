ba32be364fa8a144b8a5311d56ab5caff62568a2
DHANA VIGNESH.T <dhanavignesh610@gmail.com>
2024-07-15 11:46:11 +0530

    deploy issue fix

--- client/src/axiosAPI/axios.js
+++ client/src/axiosAPI/axios.js
@@ -1,5 +1,5 @@
     import axios from "axios"; 
-    const BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api'
+    const BASE_URL = 'http://localhost:3000/api'
 
     export default axios.create({
         baseURL : BASE_URL
