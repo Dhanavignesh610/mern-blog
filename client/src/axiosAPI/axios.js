d4b5406143bda0eef6ed46b9d8f9b2867b2f88f3
DHANA VIGNESH.T <dhanavignesh610@gmail.com>
2024-08-05 16:36:00 +0530

    production api fetch

--- client/src/axiosAPI/axios.js
+++ client/src/axiosAPI/axios.js
@@ -1,14 +1,14 @@
     import axios from "axios"; 
-    const BASE_URL = 'http://localhost:3000/api'
+    const BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api' 
 
-    export default axios.create({
-        baseURL : BASE_URL
-    }) 
+    export default axios.create({   
+        baseURL : BASE_URL  
+    })    
 
-    export const axiosPrivate = axios.create({
-        baseURL: BASE_URL,
-        headers: { 'Content-Type': 'application/json' },
-        withCredentials: true
-    });
-
+    export const axiosPrivate = axios.create({  
+        baseURL: BASE_URL,   
+        headers: { 'Content-Type': 'application/json' },   
+        withCredentials: true   
+    });  
+   
 
